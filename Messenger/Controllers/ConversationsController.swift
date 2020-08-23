af9bc90e9d57dae2749ddd142000d8f7abbe0ddf
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-23 20:43:53 -0300

    working on storyboards, ProfileVC and adding the logout functionality

--- Messenger/Controllers/ConversationsController.swift
+++ Messenger/Controllers/ConversationsController.swift
@@ -13,7 +13,7 @@ class ConversationsController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        
         
         
     }
